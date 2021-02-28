8386631fcc823d0f91842cca4c588b18a8cc0d78
Xiang Li <xiangl@node006.cm.cluster>
2021-02-28 17:26:12 -0500

    change typo in install file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-conda create -n myenv python=3.6
+conda create -n protoqa python=3.6
 source activate protoqa
 git clone git@github.com:iesl/protoqa-evaluator.git
 pip install -e protoqa-evaluator
@@ -7,6 +7,3 @@ git clone git@github.com:huggingface/transformers.git
 cd transformers
 git checkout tags/v2.1.1
 pip install -e .
-pip install tensorboardX
-cd ../
-mkdir pre_trained_model
--- run_lm_finetuning.py
+++ run_lm_finetuning.py
@@ -600,4 +600,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
